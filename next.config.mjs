ceb4b1551f5aaba358b3940047633c6052ead4ad
Rengga Prakoso Nugroho <rengganugroho@teknologipendidikan.or.id>
2024-06-10 00:20:35 +0700

    chore: add redirects to article

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/definisi-tep-2023',
+            destination: 'https://renggaprakosonugroho.my.id/definisi-terbaru-teknologi-pendidikan-aect-2023-cakupan-lebih-luas-dan-relevansi-terkini/',
+            permanent: true,
+          },
+        ]
+      },
+};
 
 export default nextConfig;
