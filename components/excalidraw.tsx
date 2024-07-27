a5f7c8e06039d6e7eb4ee4f71cb02ac1a58deab0
vzrenggamani <rengga.prakoso@gmail.com>
2024-07-28 02:37:05 +0700

    chore: fix the scroll bar in excalidraw

--- components/excalidraw.tsx
+++ components/excalidraw.tsx
@@ -7,9 +7,22 @@ import {
 
 const ExcalidrawWrapper: React.FC = () => {
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <Excalidraw />
-    </div>
+    <>
+      <style jsx global>{`
+        body,
+        html {
+          margin: 0;
+          padding: 0;
+          height: 100%;
+          width: 100%;
+          overflow: hidden;
+        }
+      `}</style>
+      <main style={{ height: "100vh", width: "100vw", margin: 0, padding: 0 }}>
+        <Excalidraw />
+      </main>
+    </>
   );
 };
+
 export default ExcalidrawWrapper;
